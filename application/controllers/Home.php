abb43e55c840706462b38639b29a3369bf6d1861
a101334 <alex.101334@hotmail.com>
2017-07-24 20:26:22 -0300

    Update readme and add home.php, message.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Message extends CI_Controller {
+class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('message_view');
+		$this->load->view('home');
 	}
 }
